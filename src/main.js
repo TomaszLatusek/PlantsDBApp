3dec1e56753b7c08fbc7c0413656fef2dd7e2037
Tomasz Latusek <tomaszlatusek.tl@gmail.com>
2022-04-30 20:19:07 +0200

    Create basic App view.

--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ import App from './App.vue';
 import router from './router';
 
 createApp(App).use(router).mount('#app');
+
+// new Vue({
+//     router,
+//     render: h => h(App),
+// }).$mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,12 @@ import HomeView from '../views/HomeView.vue';
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'home-page',
     component: HomeView,
   },
   {
     path: '/about',
-    name: 'about',
+    name: 'about-page',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
